dd0d631fc075502acb6473ed6ea274b46fdec3f2
Fouad AMANSOUR <fd.amansour@gmail.com>
2020-08-27 10:08:16 +0200

    The mouse works well in the background. However, a new bug has just appeared at this level (after a few start/stop, the counter stops responding).

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 import time
 import classes 
-# from functions import *
+import win32api
+from threading import Thread
 
 def mouseClicked(iLoopLocal,swLocal):
     if (iLoopLocal % 2)== 0:
@@ -22,10 +23,30 @@ root = tk.Tk() # initializing the tcl/tk interpreter and creating the root windo
 sw = classes.StopWatch(root)
 sw.pack(side=tk.TOP)# Tkinter Pack Geometry Manager
 
-frame = tk.Frame(root, width=100, height=100)
-frame.bind("<Button-2>", lambda x=None: mouseClicked(iLoop,sw)) #bind Python functions and methods to events
-frame.pack() # Tkinter Pack Geometry Manager
+# ----------------------
+
+
+CurrentKeyState = win32api.GetKeyState(0x04)  # "0x04" is the Virtual-Key Code of the Middle mouse button (VK_MBUTTON)
+
+
+def firstFunction():
+    while True:
+        NewKeyState = win32api.GetKeyState(0x04)
+        global CurrentKeyState
+        if NewKeyState != CurrentKeyState:  # Button state changed
+
+            if NewKeyState < 0:  # KeyState = -127 or -128
+                print('BUTTON PRESSED ---', 'NewKeyState = ', NewKeyState )
+
+            else:                # KeyState =1 or 0
+                print('BUTTON RELEASED ---', 'NewKeyState = ', NewKeyState)
+                mouseClicked(iLoop,sw)
+            CurrentKeyState = NewKeyState
+            
+        time.sleep(0.001)
+    
+thread1  = Thread(target = firstFunction)
+thread1.start()
+# root.after(1, firstFunction()) 
 
 root.mainloop() #This method will loop forever, waiting for events from the user, until it exits the program
-    
-    
